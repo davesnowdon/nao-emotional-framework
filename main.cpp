106e6167658af681626711866791bcceb18747ea
Dave Snowdon <dave@davesnowdon.com>
2014-05-04 19:39:34 +0100

    basic main

--- main.cpp
+++ main.cpp
@@ -5,8 +5,53 @@
  */
 #include <iostream>
 
+#include <boost/shared_ptr.hpp>
+
+#include <alcommon/albroker.h>
+#include <alcommon/albrokermanager.h>
+
+#include "emotion.h"
+#include "motion.h"
+#include "tts.h"
+#include "leds.h"
+
+#ifdef FRAMEWORK_IS_REMOTE
+# define ALCALL
+#else
+// when not remote, we're in a dll, so export the entry point
+# ifdef _WIN32
+#  define ALCALL __declspec(dllexport)
+# else
+#  define ALCALL
+# endif
+#endif
+
+extern "C"
+{
+  ALCALL int _createModule(boost::shared_ptr<AL::ALBroker> broker)
+  {
+    // init broker with the main broker instance
+    // from the parent executable
+    AL::ALBrokerManager::setInstance(broker->fBrokerManager.lock());
+    AL::ALBrokerManager::getInstance()->addBroker(broker);
+    // create module instances
+    AL::ALModule::createModule<Emotion>(broker, "Emotion");
+    AL::ALModule::createModule<EmotionalMotion>(broker, "EmotionalMotion");
+    AL::ALModule::createModule<EmotionalTextToSpeech>(broker, "EmotionalTextToSpeech");
+    AL::ALModule::createModule<EmotionalLeds>(broker, "EmotionalLeds");
+    return 0;
+  }
+
+  ALCALL int _closeModule(  )
+  {
+    return 0;
+  }
+};
+
+#ifdef FRAMEWORK_IS_REMOTE
 int main()
 {
   std::cout << "Hello, world" << std::endl;
   return 0;
 }
+#endif
