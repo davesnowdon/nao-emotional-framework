3cf157af7a4dcea1a4b995e8f44b1c0b151e50d3
Dave Snowdon <dave@davesnowdon.com>
2014-08-07 22:14:19 +0100

    Added interface definitions for text to speech

--- tts.cpp
+++ tts.cpp
@@ -8,70 +8,200 @@ EmotionalTextToSpeech::EmotionalTextToSpeech(boost::shared_ptr<AL::ALBroker> bro
   : AL::ALModule(broker, name)
 {
   // Describe the module here. This will appear on the webpage
-  setModuleDescription("My own custom module.");
+  setModuleDescription("Emotional wrapper for ALTextToSpeech.");
 
-  /**
-   * Define callable methods with their descriptions:
-   * This makes the method available to other cpp modules
-   * and to python.
-   * The name given will be the one visible from outside the module.
-   * This method has no parameters or return value to describe
-   * functionName(<method_name>, <class_name>, <method_description>);
-   * BIND_METHOD(<method_reference>);
-   */
-  functionName("printHello", getName(), "Print hello to the world");
-  BIND_METHOD(EmotionalTextToSpeech::printHello);
 
-  /**
-   * addParam(<attribut_name>, <attribut_descrption>);
-   * This enables to document the parameters of the method.
-   * It is not compulsory to write this line.
-   */
-  functionName("printWord", getName(), "Print a given word.");
-  addParam("word", "The word to be print.");
-  BIND_METHOD(EmotionalTextToSpeech::printWord);
+  functionName("getAvailableLanguages", getName(), "Outputs the languages installed on the system.");
+  setReturn("std::vector<std::string>", "Array of std::string that contains the languages installed on the system.");
+  BIND_METHOD(EmotionalTextToSpeech::getAvailableLanguages);
 
-  /**
-   * setReturn(<return_name>, <return_description>);
-   * This enables to document the return of the method.
-   * It is not compulsory to write this line.
-   */
-  functionName("returnTrue", getName(), "Just return true");
-  setReturn("boolean", "return true");
-  BIND_METHOD(EmotionalTextToSpeech::returnTrue);
+  functionName("getAvailableVoices", getName(), "Outputs the available voices. The returned list contains the voice IDs.");
+  setReturn("std::vector<std::string>", "Array of std::string containing the voices installed on the system.");
+  BIND_METHOD(EmotionalTextToSpeech::getAvailableVoices);
 
-  // If you had other methods, you could bind them here...
-  /**
-   * Bound methods can only take const ref arguments of basic types,
-   * or AL::ALValue or return basic types or an AL::ALValue.
-   */
+  functionName("getLanguage", getName(), "Returns the language currently used by the text-to-speech engine.");
+  setReturn("std::string", "Language of the current voice.");
+  BIND_METHOD(EmotionalTextToSpeech::getLanguage);
+
+  functionName("getParameter", getName(), "Returns the value of one of the voice parameters. The available parameters are: pitchShift, doubleVoice,doubleVoiceLevel and doubleVoiceTimeShift");
+  addParam("pParameterName", "Name of the parameter");
+  setReturn("float", "Value of the specified parameter");
+  BIND_METHOD(EmotionalTextToSpeech::getParameter);
+
+  functionName("getSupportedLanguages", getName(), "Outputs all the languages supported (may be installed or not).");
+  setReturn("std::vector<std::string>", "Array of std::string that contains all the supported languages (may be installed or not).");
+  BIND_METHOD(EmotionalTextToSpeech::getSupportedLanguages);
+
+  functionName("getVoice", getName(), "Returns the voice currently used by the text-to-speech engine.");
+  setReturn("std::string", "Name of the current voice");
+  BIND_METHOD(EmotionalTextToSpeech::getVoice);
+
+  functionName("getVolume", getName(), "Fetches the current volume the text to speech.");
+  setReturn("float", "Volume (integer between 0 and 100).");
+  BIND_METHOD(EmotionalTextToSpeech::getVolume);
+
+  functionName("loadVoicePreference", getName(), "Loads a set of voice parameters defined in a xml file contained in the preferences folder.The name of the xml file must begin with ALTextToSpeech_Voice_");
+  addParam("pPreferenceName", "Name of the voice preference.");
+  BIND_METHOD(EmotionalTextToSpeech::loadVoicePreference);
+
+  functionName("locale", getName(), "Get the locale associate to the current language.");
+  setReturn("std::string", "A string with xx_XX format (region_country)");
+  BIND_METHOD(EmotionalTextToSpeech::locale);
+
+  functionName("say", getName(), "Performs the text-to-speech operations : it takes a std::string as input and outputs a sound in both speakers. String encoding must be UTF8.");
+  addParam("stringToSay", "Text to say, encoded in UTF-8.");
+  AL::completeAndCheck<
+    EmotionalTextToSpeech,
+    const std::string&,
+    void>
+    (&EmotionalTextToSpeech::say, getCurrentMethodDescription());
+  bindMethodOverload(AL::createFunctor<EmotionalTextToSpeech,
+                     std::string,
+                     void>
+  (this, &EmotionalTextToSpeech::say));
+
+  functionName("say", getName(), "Performs the text-to-speech operations in a specific language: it takes a std::string as input and outputs a sound in both speakers. String encoding must be UTF8. Once the text is said, the language is set back to its initial value.");
+  addParam("std::string", "Text to say, encoded in UTF-8");
+  addParam("std::string", "Language used to say the text.");
+  AL::completeAndCheck<
+    EmotionalTextToSpeech,
+    const std::string&,
+    const std::string&,
+    void>
+    (&EmotionalTextToSpeech::say, getCurrentMethodDescription());
+  bindMethodOverload(AL::createFunctor<EmotionalTextToSpeech,
+                     std::string,
+                     std::string,
+                     void>
+  (this, &EmotionalTextToSpeech::say));
+
+  functionName("sayToFile", getName(), "Performs the text-to-speech operations: it takes a std::string as input and outputs the corresponding audio signal in the specified file.");
+  addParam("std::string", "Text to say, encoded in UTF-8");
+  addParam("std::string", "RAW file where to store the generated signal. The signal is encoded with a sample rate of 22050Hz, format S16_LE, 2 channels.");
+  BIND_METHOD(EmotionalTextToSpeech::sayToFile);
+
+  functionName("setLanguage", getName(), "Changes the language used by the Text-to-Speech engine. It automatically changes the voice used since each of them is related to a unique language. If you want that change to take effect automatically after reboot of your robot, refer to the robot web page (setting page).");
+  addParam("std::string", "Language name. Must belong to the languages available in TTS (can be obtained with the getAvailableLanguages method).  It should be an identifier std::string.");
+  BIND_METHOD(EmotionalTextToSpeech::setLanguage);
+
+  functionName("setLanguageDefaultVoice", getName(), "Sets a voice as the default voice for the corresponding language");
+  addParam("std::string", "The language among those available on your robot as a String");
+  addParam("std::string", " The voice among those available on your robot as a String");
+  BIND_METHOD(EmotionalTextToSpeech::setLanguageDefaultVoice);
+
+  functionName("setParameter", getName(), "Changes the parameters of the voice. ");
+  addParam("std::string", "Name of the parameter");
+  addParam("float", "Value of the parameter");
+  BIND_METHOD(EmotionalTextToSpeech::setParameter);
+
+  functionName("setVoice", getName(), "Changes the voice used by the text-to-speech engine. The voice identifier must belong to the installed voices, that can be listed using the 'getAvailableVoices' method. If the voice is not available, it remains unchanged. No exception is thrown in this case. For the time being, only two voices are available by default : Kenny22Enhanced (English voice) and Julie22Enhanced (French voice)");
+  addParam("std::string", "The voice");
+  BIND_METHOD(EmotionalTextToSpeech::setVoice);
+
+  functionName("setVolume", getName(), "Sets the volume of text-to-speech output.");
+  addParam("float", "Volume (between 0.0 and 1.0)");
+  BIND_METHOD(EmotionalTextToSpeech::setVolume);
+
+  functionName("stopAll", getName(), "This method stops the current and all the pending tasks immediately.");
+  BIND_METHOD(EmotionalTextToSpeech::stopAll);
 }
 
 EmotionalTextToSpeech::~EmotionalTextToSpeech()
 {
 }
 
-void EmotionalTextToSpeech::init()
+void
+EmotionalTextToSpeech::init()
 {
-  /**
-   * Init is called just after construction.
-   * Do something or not
-   */
-  std::cout << returnTrue() << std::endl;
+
 }
 
-
-void EmotionalTextToSpeech::printHello()
+std::vector<std::string>
+EmotionalTextToSpeech::getAvailableLanguages()
 {
-  std::cout << "Hello!" << std::endl;
 }
 
-void EmotionalTextToSpeech::printWord(const std::string &word)
+std::vector<std::string>
+EmotionalTextToSpeech::getAvailableVoices()
 {
-  std::cout << word << std::endl;
 }
 
-bool EmotionalTextToSpeech::returnTrue()
+std::string
+EmotionalTextToSpeech::getLanguage()
 {
-  return true;
+}
+
+float
+EmotionalTextToSpeech::getParameter(const std::string& pParameterName)
+{
+}
+
+std::vector<std::string>
+EmotionalTextToSpeech::getSupportedLanguages()
+{
+}
+
+std::string
+EmotionalTextToSpeech::getVoice()
+{
+}
+
+float
+EmotionalTextToSpeech::getVolume()
+{
+}
+
+void
+EmotionalTextToSpeech::loadVoicePreference(const std::string& pPreferenceName)
+{
+}
+
+std::string
+EmotionalTextToSpeech::locale()
+{
+}
+
+void
+EmotionalTextToSpeech::say(const std::string& stringToSay)
+{
+}
+
+void
+EmotionalTextToSpeech::say(const std::string& stringToSay, const std::string& language)
+{
+}
+
+void
+EmotionalTextToSpeech::sayToFile(const std::string& pStringToSay, const std::string& pFileName)
+{
+}
+
+void
+EmotionalTextToSpeech::setLanguage(const std::string& pLanguage)
+{
+}
+
+void
+EmotionalTextToSpeech::setLanguageDefaultVoice(const std::string& Language, const std::string& Voice)
+{
+}
+
+void
+EmotionalTextToSpeech::setParameter(const std::string& pEffectName, const float& pEffectValue)
+{
+}
+
+void
+EmotionalTextToSpeech::setVoice(const std::string& pVoiceID)
+{
+}
+
+void
+EmotionalTextToSpeech::setVolume(const float& volume)
+{
+}
+
+void
+EmotionalTextToSpeech::stopAll()
+{
 }
--- tts.h
+++ tts.h
@@ -29,13 +29,121 @@ public:
    */
   virtual void init();
 
-  // After that you may add all your bind method.
+    /// <summary>
+    /// Outputs the languages installed on the system.
+    /// </summary>
+    /// <returns> Array of std::string that contains the languages installed on the system. </returns>
+    std::vector<std::string> getAvailableLanguages();
 
-  // Function which prints "Hello!" on standard output
-  void printHello();
-  // Function which prints the word given on parameters
-  void printWord(const std::string &word);
-  // Function which returns true
-  bool returnTrue();
+    /// <summary>
+    /// Outputs the available voices. The returned list contains the voice IDs.
+    /// </summary>
+    /// <returns>  Array of std::string containing the voices installed on the system. </returns>
+    std::vector<std::string> getAvailableVoices();
+
+    /// <summary>
+    /// Returns the language currently used by the text-to-speech engine.
+    /// </summary>
+    /// <returns> Language of the current voice. </returns>
+    std::string getLanguage();
+
+    /// <summary>
+    /// Returns the value of one of the voice parameters. The available parameters are: "pitchShift", "doubleVoice","doubleVoiceLevel" and "doubleVoiceTimeShift"
+    /// </summary>
+    /// <param name="pParameterName"> Name of the parameter. </param>
+    /// <returns> Value of the specified parameter </returns>
+    float getParameter(const std::string& pParameterName);
+
+    /// <summary>
+    /// Outputs all the languages supported (may be installed or not).
+    /// </summary>
+    /// <returns> Array of std::string that contains all the supported languages (may be installed or not). </returns>
+    std::vector<std::string> getSupportedLanguages();
+
+    /// <summary>
+    /// Returns the voice currently used by the text-to-speech engine.
+    /// </summary>
+    /// <returns> Name of the current voice </returns>
+    std::string getVoice();
+
+    /// <summary>
+    /// Fetches the current volume the text to speech.
+    /// </summary>
+    /// <returns> Volume (integer between 0 and 100). </returns>
+    float getVolume();
+
+    /// <summary>
+    /// Loads a set of voice parameters defined in a xml file contained in the preferences folder.The name of the xml file must begin with ALTextToSpeech_Voice_
+    /// </summary>
+    /// <param name="pPreferenceName"> Name of the voice preference. </param>
+    void loadVoicePreference(const std::string& pPreferenceName);
+
+    /// <summary>
+    /// Get the locale associate to the current language.
+    /// </summary>
+    /// <returns> A string with xx_XX format (region_country) </returns>
+    std::string locale();
+
+    /// <summary>
+    /// Performs the text-to-speech operations : it takes a std::string as input and outputs a sound in both speakers. String encoding must be UTF8.
+    /// </summary>
+    /// <param name="stringToSay"> Text to say, encoded in UTF-8. </param>
+    void say(const std::string& stringToSay);
+
+    /// <summary>
+    /// Performs the text-to-speech operations in a specific language: it takes a std::string as input and outputs a sound in both speakers. String encoding must be UTF8. Once the text is said, the language is set back to its initial value.
+    /// </summary>
+    /// <param name="stringToSay"> Text to say, encoded in UTF-8. </param>
+    /// <param name="language"> Language used to say the text. </param>
+    void say(const std::string& stringToSay, const std::string& language);
+
+    /// <summary>
+    /// Performs the text-to-speech operations: it takes a std::string as input and outputs the corresponding audio signal in the specified file.
+    /// </summary>
+    /// <param name="pStringToSay"> Text to say, encoded in UTF-8. </param>
+    /// <param name="pFileName"> RAW file where to store the generated signal. The signal is encoded with a sample rate of 22050Hz, format S16_LE, 2 channels. </param>
+    void sayToFile(const std::string& pStringToSay, const std::string& pFileName);
+
+    /// <summary>
+    /// Changes the language used by the Text-to-Speech engine. It automatically changes the voice used since each of them is related to a unique language. If you want that change to take effect automatically after reboot of your robot, refer to the robot web page (setting page).
+    /// </summary>
+    /// <param name="pLanguage"> Language name. Must belong to the languages available in TTS (can be obtained with the getAvailableLanguages method).  It should be an identifier std::string. </param>
+    void setLanguage(const std::string& pLanguage);
+
+    /// <summary>
+    /// Sets a voice as the default voice for the corresponding language
+    /// </summary>
+    /// <param name="Language"> The language among those available on your robot as a String </param>
+    /// <param name="Voice"> The voice among those available on your robot as a String </param>
+    void setLanguageDefaultVoice(const std::string& Language, const std::string& Voice);
+
+    /// <summary>
+    /// Changes the parameters of the voice. The available parameters are:
+    ///  	 pitchShift: applies a pitch shifting to the voice. The value indicates the ratio between the new fundamental frequencies and the old ones (examples: 2.0: an octave above, 1.5: a quint above). Correct range is (1.0 -- 4), or 0 to disable effect.
+    ///  	 doubleVoice: adds a second voice to the first one. The value indicates the ratio between the second voice fundamental frequency and the first one. Correct range is (1.0 -- 4), or 0 to disable effect
+    ///  	 doubleVoiceLevel: the corresponding value is the level of the double voice (1.0: equal to the main voice one). Correct range is (0 -- 4).
+    ///  	 doubleVoiceTimeShift: the corresponding value is the delay between the double voice and the main one. Correct range is (0 -- 0.5)
+    ///  If the effect value is not available, the effect parameter remains unchanged.
+    /// </summary>
+    /// <param name="pEffectName"> Name of the parameter. </param>
+    /// <param name="pEffectValue"> Value of the parameter. </param>
+    void setParameter(const std::string& pEffectName, const float& pEffectValue);
+
+    /// <summary>
+    /// Changes the voice used by the text-to-speech engine. The voice identifier must belong to the installed voices, that can be listed using the 'getAvailableVoices' method. If the voice is not available, it remains unchanged. No exception is thrown in this case. For the time being, only two voices are available by default : Kenny22Enhanced (English voice) and Julie22Enhanced (French voice)
+    /// </summary>
+    /// <param name="pVoiceID"> The voice (as a std::string). </param>
+    void setVoice(const std::string& pVoiceID);
+
+    /// <summary>
+    /// Sets the volume of text-to-speech output.
+    /// </summary>
+    /// <param name="volume"> Volume (between 0.0 and 1.0). </param>
+    void setVolume(const float& volume);
+
+    /// <summary>
+    /// This method stops the current and all the pending tasks immediately.
+    /// </summary>
+    void stopAll();
 };
 #endif // EMOTIONAL_TTS_H
