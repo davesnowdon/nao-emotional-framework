7ee9085df92c170b782d4ab3de967253bbbe2418
Dave Snowdon <dave@davesnowdon.com>
2014-08-08 17:54:35 +0100

    Updated copyright

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 /*
- * Copyright (c) 2012, 2013 Aldebaran Robotics. All rights reserved.
- * Use of this source code is governed by a BSD-style license that can be
- * found in the COPYING file.
+ * Copyright (c) 2014 Dave Snowdon & Mike McFarlane
+ *
+ * Main module to run brokers remote from robot
  */
 #include <iostream>
 
